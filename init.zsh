6e6f3a28102af79af52701ddd810fca9e114bc23
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2024-04-06 00:04:01 +0900

    Create prompt style

--- init.zsh
+++ init.zsh
@@ -1,3 +1,4 @@
 source $ZSHRC_HOME/alias.zsh
 source $ZSHRC_HOME/env.zsh
+source $ZSHRC_HOME/prompt.zsh
 source $ZSHRC_HOME/zsh-functions.zsh
---
+++ prompt.zsh
@@ -0,0 +1,8 @@
+PROMPT=""
+if [ $SHLVL -gt 1 ]; then
+    PROMPT=$PROMPT"("$SHLVL") "
+fi
+if $PROMPT_USERNAME; then
+    PROMPT=$PROMPT"%F{red}%B%n%b%f@%F{yellow}%B%m%b%f:"
+fi
+PROMPT=$PROMPT"%F{blue}%B%~%b%f %# "
