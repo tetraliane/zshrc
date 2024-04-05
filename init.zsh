efcffba8429f353af8d0f9aef6853880d79fce65
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2024-04-05 23:09:39 +0900

    Set up zsh-only functions

--- init.zsh
+++ init.zsh
@@ -1,1 +1,2 @@
 source $ZSHRC_HOME/alias.zsh
+source $ZSHRC_HOME/zsh-functions.zsh
---
+++ zsh-functions.zsh
@@ -0,0 +1,8 @@
+# Emacs keymap
+bindkey -e
+
+# Spelling correction of commands
+setopt CORRECT
+
+# Completion
+autoload -Uz compinit && compinit
