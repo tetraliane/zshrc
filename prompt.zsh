7596ec55d3a4960e039235fe32923604f51fc640
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2024-04-06 21:42:09 +0900

    Remove the arrow before the prompt

--- prompt.zsh
+++ prompt.zsh
@@ -1,9 +1,11 @@
 PROMPT=""
-PROMPT=$PROMPT"%F{green}%B→%b%f "
+PROMPT=$PROMPT"["
 if [ $SHLVL -gt 1 ]; then
     PROMPT=$PROMPT"("$SHLVL") "
 fi
 if $PROMPT_USERNAME; then
     PROMPT=$PROMPT"%F{red}%B%n%b%f@%F{yellow}%B%m%b%f:"
 fi
-PROMPT=$PROMPT"%F{blue}%B%~%b%f %# "
+PROMPT=$PROMPT"%F{blue}%B%~%b%f"
+PROMPT=$PROMPT"]"
+PROMPT=$PROMPT" %# "
