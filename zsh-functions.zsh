126421d3893b7b1b232e876cd02dd00beec9614f
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2024-04-11 14:30:37 +0900

    Add history search shortcuts

--- zsh-functions.zsh
+++ zsh-functions.zsh
@@ -6,3 +6,9 @@ setopt CORRECT
 
 # Completion
 autoload -Uz compinit && compinit
+
+# History search
+bindkey '^R' history-incremental-search-backward
+bindkey '^S' history-incremental-search-forward
+bindkey '^P' history-beginning-search-backward
+bindkey '^N' history-beginning-search-forward
