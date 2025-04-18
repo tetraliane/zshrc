e6a0bdbc8d8b5ae127a88a1874a5bc6bed306479
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2025-04-18 16:07:18 +0900

    Set the max length of the prompt

--- prompt.zsh
+++ prompt.zsh
@@ -1,7 +1,11 @@
 PROMPT=""
-PROMPT=$PROMPT'%(1j.[%j] .)'
+# The user name and the host, if configured
 if $PROMPT_USERNAME; then
     PROMPT=$PROMPT"%F{red}%B%n%b%f@%F{yellow}%B%m%b%f:"
 fi
-PROMPT=$PROMPT"%F{blue}%B%~%b%f"
+# The current directory, with the length limitted
+PROMPT=$PROMPT"%F{blue}%B%50<..<%~%b%f"
 PROMPT=$PROMPT" %# "
+
+# Background jobs, if any
+RPROMPT="%(1j.[%j].)"
