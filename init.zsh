e3c2c6d551cf5639b8f67733d255ef3e160d1441
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2025-10-17 11:25:33 +0900

    Improve the prompt using subnixr/minimal

--- init.zsh
+++ init.zsh
@@ -1,4 +1,5 @@
 source $ZSHRC_HOME/env.zsh
 source $ZSHRC_HOME/alias.zsh
+source $ZSHRC_HOME/minimal/minimal.zsh
 source $ZSHRC_HOME/prompt.zsh
 source $ZSHRC_HOME/zsh-functions.zsh
--- prompt.zsh
+++ prompt.zsh
@@ -1,11 +1,16 @@
-PROMPT=""
-# The user name and the host, if configured
-if $PROMPT_USERNAME; then
-    PROMPT=$PROMPT"%F{red}%B%n%b%f@%F{yellow}%B%m%b%f:"
-fi
-# The current directory, with the length limitted
-PROMPT=$PROMPT"%F{blue}%B%50<..<%~%b%f"
-PROMPT=$PROMPT" %# "
+ELLIPSIS_CHAR="${ELLIPSIS_CHAR:-..}"
+CWD_COLOR="${CWD_COLOR:-blue}"
 
-# Background jobs, if any
-RPROMPT="%(1j.[%j].)"
+
+function cwd_trunc() {
+    local echar="$ELLIPSIS_CHAR"
+    local cwdc="$CWD_COLOR"
+    local len=$1
+
+    echo "%F{$cwdc}%B%$len<$echar<%~%b%f"
+}
+
+
+MNML_USER_CHAR='%#'
+MNML_PROMPT=(mnml_ssh 'cwd_trunc 40' mnml_status)
+MNML_RPROMPT=(mnml_git)
